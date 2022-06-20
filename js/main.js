d4b8bc9df61d126900d703840f86005dadf3b920
Kzohidova <kamolazohidova04@gmail.com>
2022-06-20 15:05:55 +0500

    Add the js part

--- js/main.js
+++ js/main.js
@@ -11,42 +11,42 @@ const elsTabLink = document.querySelectorAll('.js-tab-link');
 const elsAccordionItem = document.querySelectorAll('.accordion__item');
 const elsAccordionItemToggler = document.querySelectorAll('.accordion__item-toggler');
 
-function deactivateTabItems () {
+function deactivateTabItems() {
     elsTabsItem.forEach(function (elTabsItem) {
-        elTabsItem.classList.remove(modifiers.tabsItemActive);
+        // elTabsItem.classList.remove(modifiers.tabsItemActive);
     });
 }
 
-function deactivateTabPanels () {
+function deactivateTabPanels() {
     elsTabsPanel.forEach(function (elTabsPanel) {
-        elTabsPanel.classList.remove(modifiers.tabPanelActive);
+        // elTabsPanel.classList.remove(modifiers.tabPanelActive);
     });
 }
 
-function closeAccordionItems () {
-    elsAccordionItem.forEach(function(elAccordionItem) {
-        elAccordionItem.classList.remove(modifiers.accordionItemOpen)
+function closeAccordionItems() {
+    elsAccordionItem.forEach(function (elAccordionItem) {
+        // elAccordionItem.classList.remove(modifiers.accordionItemOpen)
     });
 }
 
 elsTabLink.forEach(function (elsTabLink) {
-    elsTabLink.addEventListener('click', function(evt) {
+    elsTabLink.addEventListener('click', function (evt) {
         // Prevent page move 
         evt.preventDefault();
 
         // Remove active class form tabs__item elements 
         deactivateTabItems();
-        
-        // Add active class to current tabs__item 
-        elsTabLink.parentElement.classList.add(modifiers.tabsItemActive);
 
-         // Remove active class form tabs__panel elements 
-         deactivateTabPanels();
+        // Add active class to current tabs__item 
+        elsTabLink.parentElement.classList.toggle(modifiers.tabsItemActive);
+
+        // Remove active class form tabs__panel elements 
+        deactivateTabPanels();
 
         //  Show active tab panel 
         // const elTargetPanel = document.querySelector(`#${elsTabLink.href.split('#')[1]}`);
         const elTargetPanel = document.querySelector(elsTabLink.dataset.tabTarget);
-        elTargetPanel.classList.add(modifiers.tabPanelActive);
+        elTargetPanel.classList.toggle(modifiers.tabPanelActive);
     });
 });
 
@@ -54,6 +54,6 @@ elsAccordionItemToggler.forEach(function (elAccordionItemToggler) {
     elAccordionItemToggler.addEventListener('click', function () {
         closeAccordionItems();
 
-        elAccordionItemToggler.closest('.accordion__item').classList.add(modifiers.accordionItemOpen)
+        elAccordionItemToggler.closest('.accordion__item').classList.toggle(modifiers.accordionItemOpen)
     });
 });
